89d9f0afa30b85663fc2c1ade392b0eb20862c98
Alex Kibler <alexkibler@me.com>
2017-10-30 12:12:37 -0400

    Add methods to parks-helpers.js

--- services/parks/parks-helpers.js
+++ services/parks/parks-helpers.js
@@ -12,6 +12,43 @@ function parksInitialFetch(req, res, next) {
     })
 }
 
+function getAlerts(req, res, next) {
+  fetch(`https://developer.nps.gov/api/v1/alerts?parkCode=${req.body.code}&limit=5&api_key=${process.env.API_KEY}`)
+    .then(response => response.json())
+    .then(jsonRes => {
+      res.locals.alertsArray = jsonRes.data
+      next()
+    }).catch((err) => {
+      console.log(err)
+      next(err)
+    })
+}
+
+function getEvents(req, res, next) {
+  fetch(`https://developer.nps.gov/api/v1/events?parkCode=${req.body.code}&limit=5&api_key=${process.env.API_KEY}`)
+    .then(response => response.json())
+    .then(jsonRes => {
+      res.locals.eventsArray = jsonRes.data
+      next()
+    }).catch((err) => {
+      console.log(err)
+      next(err)
+    })
+}
+
+function getArticles (req, res, next) {
+  fetch(`https://developer.nps.gov/api/v1/articles?limit=100&start=1790&api_key=${process.env.API_KEY}`)
+    .then(response => response.json())
+    .then(jsonRes => {
+      res.locals.tempArray = jsonRes.data
+      res.locals.articlesArray = res.locals.tempArray.reverse()
+      next()
+    }).catch((err) => {
+      console.log(err)
+      next(err)
+    })
+}
+
 module.exports = {
   parksInitialFetch: parksInitialFetch,
 }
