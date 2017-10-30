3e90ada081bacb7c83067acc07fa352c85fda534
Alex Kibler <alexkibler@me.com>
2017-10-30 11:31:56 -0400

    Update SQL request

--- models/park.js
+++ models/park.js
@@ -6,10 +6,10 @@ Park.createSeveral = (parks) => {
   return db.tx((t) => {
     const queries = parks.map((park) => {
       return db.one(`
-          INSERT INTO parks (code, name, states, lat_long, description, designation, directions_info, directions_url, url, weather_info)
-          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+          INSERT INTO parks (code, name, states, lat_long, description, designation, directions_url, url)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
           RETURNING *
-        `, [park.parkCode, park.fullName, park.states, park.latLong, park.description, park.designation, park.directionsInfo, park.directionsUrl, park.url, park.weatherInfo])
+        `, [park.parkCode, park.fullName, park.states, park.latLong, park.description, park.designation, park.directionsUrl, park.url])
     })
     return t.batch(queries)
   })
@@ -20,5 +20,7 @@ Park.findAll = () => {
       SELECT * FROM parks
     `)
 }
+
+Park.findByState = ()
 
 module.exports = Park
