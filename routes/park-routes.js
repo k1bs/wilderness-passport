85987fa24591b9ccfbbb2fb2ba42f7ae70a6e397
Alex Kibler <alexkibler@me.com>
2017-11-01 15:24:47 -0400

    make park-index login required

--- routes/park-routes.js
+++ routes/park-routes.js
@@ -7,7 +7,7 @@ const authHelpers = require('../services/auth/auth-helpers')
 const geoHelpers = require('../services/geo/geo-helpers')
 const reqHelpers = require('../services/req/req-helpers')
 
-parkRouter.get('/', parksController.index)
+parkRouter.get('/', authHelpers.loginRequired, parksController.index)
 parkRouter.get('/parksinit', authHelpers.loginRequired, parksHelpers.parksInitialFetch, parksController.initialInit)
 
 parkRouter.post('/search', authHelpers.loginRequired, reqHelpers.nullToJunk, geoHelpers.getStateFromAddress, parksController.findByStateOrName)
